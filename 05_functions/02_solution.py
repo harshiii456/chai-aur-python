cfdc30209016a32d2c122d0b6c828e35c4a06916
Harshita <harshita.g.2k@gmail.com>
2026-01-28 13:40:37 +0530

    05_functions added

---
+++ 05_functions/02_solution.py
@@ -0,0 +1,5 @@
+def sum(a, b):
+  return a + b
+
+result = sum(3, 4)
+print(result)  # Output: 7  
---
+++ 05_functions/06_solution.py
@@ -0,0 +1,6 @@
+def cube(num)
+    return num ** 3
+
+cube = lambda num: num ** 3
+
+print(cube(3))  # Output: 27
